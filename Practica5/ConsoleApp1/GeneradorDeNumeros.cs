83c3b5d9de130b275607480b014eae227976f5e8
Antonio <antoniolaguna97@gmail.com>
2018-09-04 16:18:42 -0500

    practica 5

---
+++ Practica5/ConsoleApp1/GeneradorDeNumeros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GeneradorDeNumeros
+    {
+        int[] numeros = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+        public int GetNumero(int indice)
+        {
+            if (indice < 0 || indice >= numeros.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return numeros[indice];
+        }
+    }
+}
---
+++ Practica5/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1 = 3000;
+            int number2 = 0;
+            try
+            {
+                Console.WriteLine(number1 / number2);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division of {0} by zero.", number1);
+            }
+
+
+            GeneradorDeNumeros Gnum = new GeneradorDeNumeros();
+
+            Console.WriteLine(Convert.ToString(Gnum.GetNumero(10)));
+            Console.ReadKey();
+        }
+    }
+}
